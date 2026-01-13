0d42233a86781a5db0289f393983118551e2d0ba
Mahmood.Q.K <55846115+Abafariduddin1@users.noreply.github.com>
2026-01-13 00:05:10 +0000

    Update scroll.js

--- scroll.js
+++ scroll.js
@@ -1,13 +1,14 @@
-<script>
 const elements = document.querySelectorAll('.fade-in');
 
-const observer = new IntersectionObserver(entries => {
-  entries.forEach(entry => {
-    if (entry.isIntersecting) {
-      entry.target.classList.add('visible');
-    }
-  });
-}, { threshold: 0.3 });
+const observer = new IntersectionObserver(
+  entries => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add('visible');
+      }
+    });
+  },
+  { threshold: 0.25 }
+);
 
 elements.forEach(el => observer.observe(el));
-</script>
